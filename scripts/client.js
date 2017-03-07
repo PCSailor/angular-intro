40ec5d380925fff9f24ca9a53b7c40e9d7f617ab
Phil Curtis <Phil@PhilCurtis.io>
2017-03-06 19:02:03 -0600

    basic mode working

--- scripts/client.js
+++ scripts/client.js
@@ -1,24 +1,23 @@
 var phirePhiterApp = angular.module('PhirePhiterApp', []);
-
 phirePhiterApp.controller('PhirePhiterController', function(){
   console.log('PhirePhiter controller loaded');
-
   var self = this; // holds the value of this
-
+  console.log(this);
+  self.newPerson = {};
   self.people = [
     {
-      "firstName": "Luke",
-      "lastName": "Schlangen",
-      "git_username": "LukeSchlangen",
-      "shoutout": "Thanks for being awesome during the angular lecture today."
+      // "firstName": "Luke",
+      // "lastName": "Schlangen",
+      // "git_username": "LukeSchlangen",
+      // "shoutout": "Thanks for being awesome during the angular lecture today."
+      firstName: "Luke",
+      lastName: "Schlangen",
+      git_username: "LukeSchlangen",
+      shoutout: "Thanks for being awesome during the angular lecture today."
     }
   ];
-
-  self.newPerson = {};
-
   self.createPerson = function(){
     console.log(self.newPerson);
     self.people.push(angular.copy(self.newPerson));
   };
-
 });
